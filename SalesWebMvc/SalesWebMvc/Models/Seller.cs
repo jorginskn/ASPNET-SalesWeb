24bae237c7a92c65f8d6e37e4a91a6604d747f83
Jorge Martins <77112497+jorginskn@users.noreply.github.com>
2023-08-22 22:42:48 -0300

    feat: SW-5 => Criação da classes SalesRecord e Seller com suas propriedades

---
+++ SalesWebMvc/SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,40 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double BaseSalary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BaseSalary = baseSalary;
+            BirthDate = birthDate;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord Salesrecord)
+        {
+            Sales.Add(Salesrecord);
+        }
+
+        public void RemoveSales(SalesRecord Salesrecord)
+        {
+            Sales.Remove(Salesrecord);
+        }
+
+        public double TotalSales(DateTime initial, DateTime Final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= Final).Sum(sr => sr.Amount);
+        }
+    }
+}
